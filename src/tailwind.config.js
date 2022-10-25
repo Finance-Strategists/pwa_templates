9666c1df7febd176b5a3b8d951c37c7e72446192
Jair Orduña <jair_jtgdrummer13@yahoo.com>
2022-10-25 20:31:06 +0800

    Branch Updates
    Adding main files to the branch

---
+++ src/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#015768',
+        secondary: '#015363',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwindcss-children'),
+  ],
+}
